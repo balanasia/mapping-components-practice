71120f50453da045caa97b091acff4716b5021b6
Anastasiia <41988936+balanasia@users.noreply.github.com>
2022-07-20 08:35:47 -0500

    Create Emoji components and export it. Import Emoji.jsx and emojipedia.jsx into index.js. Create a list function and use it inside the App function

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -1,23 +1,24 @@
 const emojipedia = [
   {
     id: 1,
-    emoji: "💪",
-    name: "Tense Biceps",
+    emoji: "🥺",
+    name: "Face with begging eyes",
     meaning:
-      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+      "“The big eyes are shining, devoted facial expression. The emoji is entreatingly begging for something: wants to borrow some money or the car. If you cannot get ahead by asking for something, puppy dog eyes are the last resort to get what you want."
   },
   {
     id: 2,
-    emoji: "🙏",
-    name: "Person With Folded Hands",
+    emoji: "🤠",
+    name: "Face With Cowboy Hat",
     meaning:
-      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+      "Howdy! Typical American smiley with cowboy hat from the Wild West. Stands for freedom, nature, and hard work."
   },
   {
     id: 3,
-    emoji: "🤣",
-    name: "Rolling On The Floor, Laughing",
-    meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    emoji: "🧐",
+    name: "Face With Monocle",
+    meaning: "This emoji indicates that something is sus."
   }
 ];
+
+export default emojipedia;
